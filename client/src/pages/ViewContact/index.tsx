34b1244333bd53641fbcdde05e517a0f5fdf2da6
Vladyslav <prostoandrei9@gmail.com>
2023-07-07 17:12:07 +0300

    bio and email to copy

--- client/src/pages/ViewContact/index.tsx
+++ client/src/pages/ViewContact/index.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentPage } from "../../redux/slices/currentPage/slice";
 import { selectCurrentPage } from "../../redux/slices/currentPage/selectors";
-import { ContactItemType } from "../../types";
+import { ContactDataType, ContactItemType } from "../../types";
 import TimeAgo from "../../components/TimeAgo";
 import { selectUser } from "../../redux/slices/user/selectors";
 
@@ -11,12 +11,13 @@ const ViewContact: React.FC = () => {
   const dispatch = useDispatch();
   const contactId = useSelector(selectCurrentPage).id;
   const myId = useSelector(selectUser).id;
-  const [contactData, setContactData] = React.useState<ContactItemType>({
+  const [contactData, setContactData] = React.useState<ContactDataType>({
     id: contactId,
     first_name: "",
     last_name: "",
     profile_photo: "",
     last_seen: "",
+    email: "",
   });
   const [isMuted, setIsMuted] = React.useState(false);
   const [isDeleting, setIsDeleting] = React.useState(false);
@@ -33,6 +34,7 @@ const ViewContact: React.FC = () => {
           last_name: data.last_name,
           profile_photo: data.profile_photo,
           last_seen: data.last_seen,
+          email: data.email,
         });
       };
       getContact();
@@ -68,6 +70,23 @@ const ViewContact: React.FC = () => {
     }
   };
 
+  const onCopyEmail = () => {
+    // Create a temporary input element
+    const input = document.createElement("input");
+    input.value = contactData.email;
+    document.body.appendChild(input);
+
+    // Select the text in the input field
+    input.select();
+    input.setSelectionRange(0, input.value.length);
+
+    // Copy the selected text to the clipboard
+    document.execCommand("copy");
+
+    // Clean up by removing the temporary input
+    document.body.removeChild(input);
+  };
+
   return (
     <div className="flex flex-col w-full">
       <header className="h-14 flex flex-nowrap w-full bg-slate-800 border-b border-slate-600 items-center">
@@ -96,8 +115,8 @@ const ViewContact: React.FC = () => {
                 className="m-auto"
                 version="1.1"
                 xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
+                width="24"
+                height="24"
                 viewBox="0 0 32 32"
                 fill="rgb(59 130 246)"
               >
@@ -115,9 +134,9 @@ const ViewContact: React.FC = () => {
                 <svg
                   className="m-auto"
                   xmlns="http://www.w3.org/2000/svg"
-                  height="32"
+                  height="24"
                   viewBox="0 -960 960 960"
-                  width="32"
+                  width="24"
                   fill="rgb(59 130 246)"
                 >
                   <path d="M160-200v-60h84v-306q0-84 49.5-149.5T424-798v-29q0-23 16.5-38t39.5-15q23 0 39.5 15t16.5 38v29q81 17 131 82.5T717-566v306h83v60H160ZM480-80q-32 0-56-23.5T400-160h160q0 33-23.5 56.5T480-80Z" />
@@ -126,9 +145,9 @@ const ViewContact: React.FC = () => {
                 <svg
                   className="m-auto"
                   xmlns="http://www.w3.org/2000/svg"
-                  height="32"
+                  height="24"
                   viewBox="0 -960 960 960"
-                  width="32"
+                  width="24"
                   fill="rgb(59 130 246)"
                 >
                   <path d="M717-332 313-736q27-26 54.5-40.5T424-798v-26q0-23 16.5-39.5T480-880q23 0 39.5 16.5T536-824v26q78 17 129.5 82T717-566v234ZM160-200v-60h84v-315q0-14 1.5-27t5.5-26L75-805l42-42 726 727-42 42-122-122H160ZM480-80q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80Z" />
@@ -142,9 +161,9 @@ const ViewContact: React.FC = () => {
               <svg
                 className="m-auto"
                 xmlns="http://www.w3.org/2000/svg"
-                height="32"
+                height="24"
                 viewBox="0 -960 960 960"
-                width="32"
+                width="24"
                 fill="rgb(59 130 246)"
               >
                 <path d="M727-80q-47.5 0-80.75-33.346Q613-146.693 613-194.331q0-6.669 1.5-16.312T619-228L316-404q-15 17-37 27.5T234-366q-47.5 0-80.75-33.25T120-480q0-47.5 33.25-80.75T234-594q23 0 44 9t38 26l303-174q-3-7.071-4.5-15.911Q613-757.75 613-766q0-47.5 33.25-80.75T727-880q47.5 0 80.75 33.25T841-766q0 47.5-33.25 80.75T727-652q-23.354 0-44.677-7.5T646-684L343-516q2 8 3.5 18.5t1.5 17.741q0 7.242-1.5 15Q345-457 343-449l303 172q15-14 35-22.5t46-8.5q47.5 0 80.75 33.25T841-194q0 47.5-33.25 80.75T727-80Z" />
@@ -156,20 +175,45 @@ const ViewContact: React.FC = () => {
             onClick={onDeleteContact}
             className="bg-slate-800 p-4 w-full rounded-xl m-3"
           >
-            <picture className="m-auto w-auto">
+            <button className="m-auto w-auto">
               <svg
                 className="m-auto"
                 xmlns="http://www.w3.org/2000/svg"
-                height="32"
+                height="24"
                 viewBox="0 -960 960 960"
-                width="32"
+                width="24"
                 fill="red"
               >
                 <path d="M261-120q-24 0-42-18t-18-42v-570h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm106-146h60v-399h-60v399Zm166 0h60v-399h-60v399Z" />
               </svg>
-            </picture>
-            <p className="text-red-500">{isDeleting ? "Deleting..." : "Delete contact"}</p>
+            </button>
+            <p className="text-red-500">
+              {isDeleting ? "Deleting..." : "Delete contact"}
+            </p>
           </button>
+        </div>
+        <div className="w-full max-w-3xl m-4 bg-slate-800 p-4 rounded-xl ">
+          <div className="m-3 text-blue-500 border-b pb-3  border-slate-600">
+            <h5>email</h5>
+            <div className="flex justify-between">
+              <p className="text-white">{contactData.email}</p>
+              <button onClick={onCopyEmail} className="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 -960 960 960"
+                  width="24"
+                  fill="rgb(59 130 246)"
+                >
+                  <path d="M180-81q-24 0-42-18t-18-42v-603h60v603h474v60H180Zm120-120q-24 0-42-18t-18-42v-560q0-24 18-42t42-18h440q24 0 42 18t18 42v560q0 24-18 42t-42 18H300Zm0-60h440v-560H300v560Zm0 0v-560 560Z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="m-3 text-blue-500  pb-3">
+            <h5>bio</h5>
+            <p className="text-white">I'm not a bad person</p>
+          </div>
         </div>
       </main>
     </div>
